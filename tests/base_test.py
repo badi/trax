84f4ef88e83a092da2d7fecb4c75bdf8a0cd5bc9
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-11-12 16:20:08 -0500

    flesh out dummy test for AbstractTransactional

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,1 +1,46 @@
 
+
+from trax import base
+
+import os, unittest
+
+class ImplTransactional(base.AbstractTransactional):
+
+	def checkpoint(self, value):
+		self._checkpoint_fd.write(str(value))
+
+	def log(self, value):
+		self._log_fd.write(str(value) + '\n')
+
+
+def checkpoint_handler(path):
+	with open(path) as fd:
+		return fd.read()
+
+def log_handler(obj, path):
+	with open(path) as fd:
+		return [obj] + fd.readlines()
+
+
+class TestBaseTransactional(unittest.TestCase):
+
+	def setUp(self):
+		self.cpt_path = 'transactional.cpt'
+		self.log_path = 'transactional.log'
+		self.trax = ImplTransactional(checkpoint=self.cpt_path, log=self.log_path)
+
+	def tearDown(self):
+		self.trax.close()
+		os.unlink(self.cpt_path)
+		os.unlink(self.log_path)
+
+	def test_checkpoint(self):
+		cpt = 'hello'
+		log = 'world'
+
+		with self.trax as t:
+			t.checkpoint(cpt)
+			t.log(log)
+
+		recovered = self.trax.recover(checkpoint_handler=checkpoint_handler, log_handler=log_handler)
+		self.assertTrue(recovered == [cpt, log + '\n'])
