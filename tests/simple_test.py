472fc2267926e8c685d363b57a33de5128b1774f
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-12-02 15:44:12 -0500

    update tests

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -24,27 +24,16 @@ class  TestSimple(unittest.TestCase):
 
 	def test_example(self):
 
-		transactional = trax.SimpleTransactional(pickleprotocol=-1)
-		with transactional as trx:
-			state = []
-			for i in xrange(1000):
-				v = random.randint(0,100000)
-				state.append(v)
-				trx.log(v)
-				if i % 50 == 0:
-					trx.checkpoint(state)
+		trx = trax.SimpleTransactional(pickleprotocol=-1)
+		state = []
+		for i in xrange(1000):
+			v = random.randint(0,100000)
+			state.append(v)
+			trx.log(v)
+			if i % 50 == 0:
+				trx.checkpoint(state)
 
-		def log_handler(obj, fd):
-			while True:
-				try:
-					v = pickle.load(fd)
-					obj.append(v)
-				except EOFError:
-					break
-			return obj
-
-		with transactional as trx:
-			recovered = trx.recover(log_handler)
+		recovered = trx.recover(lambda o, v: o.append(v))
 
 		self.assertTrue( state == recovered )
 
